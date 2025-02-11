1f3255a4ade3c3f907af3046e4619dd900a32328
Sreeram Marimuthu <158862264+sreerammarimuthu@users.noreply.github.com>
2025-02-11 16:46:28 +0000

    Rename next.config.js for deployment

---
+++ next.config.js
@@ -0,0 +1,13 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
+  trailingSlash: true,
+}
+
+module.exports = nextConfig
+
