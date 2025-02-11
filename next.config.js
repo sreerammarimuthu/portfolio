6ba1edb38e5d4e6b67a38fa91e87e18627a90458
Sreeram Marimuthu <158862264+sreerammarimuthu@users.noreply.github.com>
2025-02-11 16:58:10 +0000

    Fix Next.js configuration for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,13 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
-  images: {
-    unoptimized: true,
-  },
-  basePath: "/portfolio",
-  assetPrefix: "/portfolio/",
-  trailingSlash: true,
-}
+  output: 'export', // Ensures static export
+  basePath: '/portfolio', // Ensures paths are correctly set for GitHub Pages
+  trailingSlash: true, // Ensures all paths have a trailing slash for GitHub Pages compatibility
+};
 
-module.exports = nextConfig
-
+module.exports = nextConfig;
