6faa3ac2926f793233201ec86ed5a2b56a87f989
Sreeram Marimuthu <158862264+sreerammarimuthu@users.noreply.github.com>
2025-02-11 16:29:40 +0000

    Update configuration for GitHub Pages

--- v0-user-next.config.js
+++ v0-user-next.config.js
@@ -4,8 +4,9 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
-  assetPrefix: "/portfolio/",
   basePath: "/portfolio",
+  assetPrefix: "/portfolio/",
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
